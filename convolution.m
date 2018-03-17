eb26836a9d7ad0c44c0e69a1ef4ba6d47c633c9b
Hovhannes Khachikyan <hovo.khc@gmail.com>
2018-03-17 15:21:28 -0400

    Proof of the convolution prop

---
+++ convolution.m
@@ -0,0 +1,29 @@
+% Function 1
+n = 0:1:10;
+a = 0.5;
+x1_n = a.^n;
+w = 2*pi * (0:(1024-1)) / 1024;
+w_prime = unwrap(fftshift(w) - 2*pi);
+
+% Function 2
+x2_n = sin(n);
+
+% Product of DTFT's both signals 
+x1_dtft = abs(dtft(x1_n));
+x2_dtft = abs(dtft(x2_n));
+x_rhs = x1_dtft .* x2_dtft;
+
+% Compute onvolution x1_n * x2_n
+x_lhs = conv(x1_n, x2_n);
+
+% Plot
+figure
+subplot(2,1,1);
+plot(w_prime, x_rhs);
+xlabel('frequency f');
+ylabel('magnitude');
+
+subplot(2,1,2);
+plot(w_prime, abs(dtft(x_lhs)));
+xlabel('frequency f');
+ylabel('magnitude');
