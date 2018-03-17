8abac43476739dcd7a418dccd8f4e5d8879263de
Hovhannes Khachikyan <hovo.khc@gmail.com>
2018-03-17 14:08:12 -0400

    Proof of Linearity Property of DTFT

---
+++ Linearity.m
@@ -0,0 +1,36 @@
+% Function 1
+n = 0:1:10;
+a = 0.5;
+x1_n = a.^n;
+w = 2*pi * (0:(1024-1)) / 1024;
+w_prime = unwrap(fftshift(w) - 2*pi);
+
+% Function 2
+x2_n = sin(n);
+
+% Constants a1, a2
+a1 = 2;
+a2 = 5;
+
+% Find the DTFT of both functions
+x1_dtft = dtft(x1_n);
+x2_dtft = dtft(x2_n);
+x_rhs = abs((a1 * x1_dtft) + (a2 * x2_dtft));
+
+% Linearity proof
+x1_prime = a1 * x1_n;
+x2_prime = a2 * x2_n;
+x_lhs = abs(dtft(x1_prime + x2_prime));
+
+% Plot
+figure
+subplot(2,1,1);
+plot(w_prime, x_rhs);
+xlabel('frequency f');
+ylabel('magnitude');
+
+subplot(2,1,2);
+plot(w_prime, x_lhs);
+xlabel('frequency f');
+ylabel('magnitude');
+
