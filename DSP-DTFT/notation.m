f073ccba24b409c30d1b15b6b701b9bab74552fe
khachikyan <hovo.khc@gmail.com>
2018-03-31 14:55:57 -0400

    added fftshift on phase and angle

--- DSP-DTFT/notation.m
+++ DSP-DTFT/notation.m
@@ -26,13 +26,13 @@ ylabel('magnitude');
 figure
 subplot(2,1,1);
 x2_w = dtft(x2, 1024);
-plot(w_prime, abs(x2_w));
+plot(w_prime, abs(fftshift(x2_w)));
 title('Magnitude of x2(n)');
 xlabel('frequency');
 ylabel('magnitude');
 
 subplot(2,1,2);
-plot(w_prime, imag(x2_w));
+plot(w_prime, imag(fftshift(x2_w)));
 title('Angle of x2(n)');
 xlabel('frequency');
 ylabel('magnitude');
