2f3cda3e62b7a7519df52a541b3820a43c5f913b
khachikyan <hovo.khc@gmail.com>
2018-03-31 14:40:40 -0400

    DTFT alg fix

--- DSP-DTFT/dtft.m
+++ DSP-DTFT/dtft.m
@@ -1,14 +1,11 @@
 %{
 Compute the DTFT of singal x with N points
 %}
-function x_w = dtft(x,N)
-    % Initialize empty array for freq spec
+function x_w= dtft(x,N)
     x_w = zeros(1,N);
-    % Iterate K times, where k=1...N
     for k=1:N
-       % Iterate length(x) times
-       for n=1:length(x)
-            x_w(k) =  x_w(k) + x(n)*exp(1i*-2*pi*(n-1)*(k-1)/length(x));
-       end
+        for m = 1:length(x)
+            x_w(k) = x_w(k) + x(m)*exp((1i)*-2*pi*(m-1)*(k-1)/N);
+        end
     end
 
